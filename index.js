d00423db210e15735f964bcb4b94f8a83d1e3dc6
David Thai <david.thai@dafuloth.me>
2018-08-28 05:42:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(deliLine, customerName) {
 
 function nowServing(line) {
   if (line.length>0) {
-    return "Currently serving " + line.shift() + "."
+    return `Currently serving ${line.shift()}.`
   } else {
     return "There is nobody waiting to be served!"
   }
